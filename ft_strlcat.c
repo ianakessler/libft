f365d1983bbc888aa32d31f146a1927505d78cc8
Ian Aratangy Kessler <iaratang@c3r2p3.42sp.org.br>
2025-07-24 18:37:40 -0300

    parte 1 fim

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: iaratang <iaratang@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/23 17:52:22 by iaratang          #+#    #+#             */
-/*   Updated: 2025/07/24 17:57:46 by iaratang         ###   ########.fr       */
+/*   Updated: 2025/07/24 18:36:46 by iaratang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,18 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 {
 	size_t	dst_len;
 	size_t	src_len;
+	size_t	i;
 
 	src_len = ft_strlen(src);
 	dst_len = ft_strlen(dst);
-	if (dst_len >= dstsize)
-		dst_len = dstsize;
-	if (dst_len == dstsize)
+	if (dstsize <= dst_len)
 		return (dstsize + src_len);
-	if (src_len < dstsize - dst_len)
-		ft_memcpy(&dst[dst_len], src, src_len + 1);
-	else
+	i = 0;
+	while (src[i] != '\0' && i < dstsize - dst_len - 1)
 	{
-		ft_memcpy(&dst[dst_len], src, dstsize - src_len -1);
-		dst[dstsize - 1] = '\0';
+		dst[dst_len + i] = src[i];
+		i++;
 	}
+	dst[dst_len + i] = '\0';
 	return (dst_len + src_len);
 }	
