a1caf73aa126433582cf36f1b15b2545f79c92ff
Ian Aratangy Kessler <iaratang@c3r3p3.42sp.org.br>
2025-07-30 17:26:23 -0300

    correção da norma de: ft_strjoin.c, ft_strtrim.c, ft_substr.c

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,16 +6,16 @@
 /*   By: iaratang <iaratang@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 22:49:53 by iaratang          #+#    #+#             */
-/*   Updated: 2025/07/30 16:35:52 by iaratang         ###   ########.fr       */
+/*   Updated: 2025/07/30 17:18:47 by iaratang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-static char *s1_trimmed(const char *s1, size_t start, size_t len);
-static int  to_trim(const char c, const char *set);
+static char	*s1_trimmed(const char *s1, size_t start, size_t len);
+static int	to_trim(const char c, const char *set);
 
-char    *ft_strtrim(const char *s1, const char *set)
+char	*ft_strtrim(const char *s1, const char *set)
 {
 	int	i;
 	int	j;
@@ -28,21 +28,21 @@ char    *ft_strtrim(const char *s1, const char *set)
 		i++;
 	while (to_trim(s1[j], set))
 		j--;
-	return (s1_trimmed(s1, i , j - (i - 1)));
+	return (s1_trimmed(s1, i, j - (i - 1)));
 }
 
-static char *s1_trimmed(const char *s1, size_t start, size_t len)
+static char	*s1_trimmed(const char *s1, size_t start, size_t len)
 {
 	char	*str;
 	size_t	i;
-	
+
 	i = 0;
 	if (len <= 0 || start >= ft_strlen(s1))
 		return (ft_strdup(""));
 	str = ft_calloc(len + 1, sizeof(char));
-	if(!str)
+	if (!str)
 		return (NULL);
-	while ( i < len)
+	while (i < len)
 	{
 		str[i] = s1[start + i];
 		i++;
@@ -50,16 +50,16 @@ static char *s1_trimmed(const char *s1, size_t start, size_t len)
 	return (str);
 }
 
-static int  to_trim(const char c, const char *set)
+static int	to_trim(const char c, const char *set)
 {
-    int i;
+	int	i;
 
-    i = 0;
-    while (set[i])
-    {
-        if (set[i] ==c )
-            return (1);
-        i++;
-    }
-    return (0);
+	i = 0;
+	while (set[i])
+	{
+		if (set[i] == c)
+			return (1);
+		i++;
+	}
+	return (0);
 }
