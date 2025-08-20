79eacd15229bff4c3205c532f6edcecd7b0233f8
Ian Aratangy Kessler <iaratang@c2r4p1.42sp.org.br>
2025-08-20 14:14:57 -0300

    l

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,7 +6,7 @@
 /*   By: iaratang <iaratang@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 22:49:53 by iaratang          #+#    #+#             */
-/*   Updated: 2025/07/30 17:18:47 by iaratang         ###   ########.fr       */
+/*   Updated: 2025/08/20 14:14:40 by iaratang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,34 +24,7 @@ char	*ft_strtrim(const char *s1, const char *set)
 	j = ft_strlen(s1) - 1;
 	if (j + 1 == 0)
 		return (ft_strdup(""));
-	while (to_trim(s1[i], set))
-		i++;
-	while (to_trim(s1[j], set))
-		j--;
-	return (s1_trimmed(s1, i, j - (i - 1)));
-}
-
-static char	*s1_trimmed(const char *s1, size_t start, size_t len)
-{
-	char	*str;
-	size_t	i;
-
-	i = 0;
-	if (len <= 0 || start >= ft_strlen(s1))
-		return (ft_strdup(""));
-	str = ft_calloc(len + 1, sizeof(char));
-	if (!str)
-		return (NULL);
-	while (i < len)
-	{
-		str[i] = s1[start + i];
-		i++;
-	}
-	return (str);
-}
-
-static int	to_trim(const char c, const char *set)
-{
+	whil
 	int	i;
 
 	i = 0;
